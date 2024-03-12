45c2f4f465d74d1a4490af30af7d462eb3132de2
Stella Laurenzo <slaurenz@amd.com>
2024-03-11 21:35:43 -0700

    Build packages as part of nightly. (#4)

--- fetch_sources.sh
+++ fetch_sources.sh
@@ -7,10 +7,11 @@ sources_dir="$this_dir/sources"
 
 mkdir -p "$sources_dir"
 cd "$sources_dir"
-repo init -u https://github.com/RadeonOpenCompute/ROCm.git
-repo sync -j16
+repo init -u https://github.com/RadeonOpenCompute/ROCm.git --depth=1
+repo sync -j16 \
+  clr HIP llvm-project rocm-cmake rocm-core ROCR-Runtime ROCT-Thunk-Interface
 
 # LLVM needs to be on the amd-staging branch.
 cd llvm-project
-git fetch --all
+git fetch rocm-org amd-staging --depth=1
 git checkout rocm-org/amd-staging
