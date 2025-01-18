ffa23737d4ca2d91ffc3d7eed0dbc41f1da3d6c9
Stella Laurenzo <stellaraccident@gmail.com>
2025-01-17 18:07:49 -0800

    Populate submodules

--- build_tools/fetch_sources.py
+++ build_tools/fetch_sources.py
@@ -41,6 +41,8 @@ def run(args):
     )
     exec(["repo", "sync", "-j16"] + args.projects, cwd=repo_dir)
 
+    populate_ancillary_sources(args)
+
     # Patches.
     if not args.no_patch:
         apply_patches(args)
@@ -50,6 +52,22 @@ def apply_patches(args):
     # TODO: Can just merge this script in here if it survives.
     script = Path(__file__).resolve().parent / "apply_patches.sh"
     exec([script], cwd=args.dir)
+
+
+def populate_ancillary_sources(args):
+    """Various subprojects have their own mechanisms for populating ancillary sources
+    needed to build. There is often something in CMake that attempts to automate it,
+    but it is also often broken. So we just do the right thing here as a transitionary
+    step to fixing the underlying software packages."""
+    populate_submodules_if_exists(args.dir / "rocprofiler-register")
+
+
+def populate_submodules_if_exists(git_dir: Path):
+    if not git_dir.exists():
+        print(f"Not populating submodules for {git_dir} (does not exist)")
+        return
+    print(f"Populating submodules for {git_dir}:")
+    exec(["git", "submodule", "update", "--init"], cwd=git_dir)
 
 
 def main(argv):
