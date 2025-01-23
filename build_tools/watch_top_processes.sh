87f69300cfd6af2cb79c3017a325d19252bd0fd1
Stella Laurenzo <slaurenz@amd.com>
2025-01-22 19:26:37 -0800

    Add a background script to periodically report top background processes. (#22)
    The ROCM build includes a fair number of multi-minute link steps, etc
    and this can be a blackhole when looking at CI logs. Add a script that
    periodically reports.

---
+++ build_tools/watch_top_processes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Reports top processes periodically. Meant to be run in the background before
+# starting a build with high latency tasks so that some progress can be seen.
+
+function cycle() {
+  echo ""
+  echo ":::: TOP PROCESSES $(date) ::::"
+  ps --no-headers aux | sort -nrk 3,3 | head -n 5 | cut -c -256
+  echo ""
+  echo ""
+}
+
+while true; do
+  # Capture content and print in one write to attempt to avoid tearing on
+  # rapidly moving terminals.
+  report="$(cycle)"
+  echo "$report" 1>&2
+  sleep 30
+done
