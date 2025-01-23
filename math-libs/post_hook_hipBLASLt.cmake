3e41ed32315aa8e09f2f7d924eaa925b647e1adc
Stella Laurenzo <slaurenz@amd.com>
2025-01-23 15:47:13 -0800

    Add hipblaslt to the project. (#24)
    * New cache variable THEROCK_ENABLE_MATH_LIBS.
    * Adds hipBLAS-common and hipBLASLt sub-projects.
    * Includes a hipBLASLt patch needed to make the library reliably
    buildable without an /opt/rocm based build (will upstream).

---
+++ math-libs/post_hook_hipBLASLt.cmake
@@ -0,0 +1,9 @@
+list(APPEND CMAKE_MODULE_PATH "${THEROCK_SOURCE_DIR}/cmake")
+include(therock_rpath)
+
+therock_set_install_rpath(
+  TARGETS
+    hipblaslt
+  PATHS
+    .
+)
---
+++ math-libs/pre_hook_hipBLASLt.cmake
@@ -0,0 +1,3 @@
+# Tensile needs to be explicitly configured with boilerplate flags needed to
+# make the toolchain work. These come from our toolchain init.
+set(Tensile_TOOLCHAIN_FLAGS "${CMAKE_CXX_FLAGS_INIT}")
