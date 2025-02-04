db5313ec8a282ae603a3d59ccee2335a36aaf6e1
Stella Laurenzo <stellaraccident@gmail.com>
2025-02-03 19:19:30 -0800

    Fix GPU_TARGETS and rocblas roctracer depend

--- comm-libs/pre_hook_rccl.cmake
+++
@@ -1,6 +0,0 @@
-# RCCL uses "GPU_TARGETS", not "AMDGPU_TARGETS" like we automatically pass.
-# Since this is notoriously hard to pass from the command line and get escaping
-# right, we just short-circuit it here and force. If this ever gets renamed
-# to AMDGPU_TARGETS like the rest, this can be removed.
-set(GPU_TARGETS "${AMDGPU_TARGETS}" CACHE STRING "From super-project" FORCE)
-unset(AMDGPU_TARGETS)
