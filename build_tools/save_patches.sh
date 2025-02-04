8e0e7efbe244912b881247bde133a35e6630b198
Stella Laurenzo <stellaraccident@gmail.com>
2025-02-03 18:12:00 -0800

    Refix hipblaslt again.

--- build_tools/save_patches.sh
+++ build_tools/save_patches.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # Saves patches that are maintained locally to make TheRock work with a given
 # branch.
-# Usage: save_patches.sh base_tag project_name patches_dir
+# Usage: save_patches.sh base_tag project_name patch_subdir
+#
+# Example:
+#   save_patches.sh m/amd-mainline hipBLASLt amd-mainline
+#
+# This will save patch files for all commits to hipBLASLt since the commit
+# "m/amd-mainline". Patch files will be saved in patches/amd-mainline/hipBLASLT
+#
 # The base_tag must be checked out on the current branch. All commits above it
 # will be dumped as patches.
 set -euo pipefail
 
-repo_dir="$(pwd)"
 this_dir="$(cd $(dirname $0) && pwd)"
 root_dir="$(cd $this_dir/.. && pwd)"
 
@@ -16,17 +22,17 @@ version_tag="$1"
 shift
 project_name="$1"
 shift
-patch_dir="$1"
+patch_subdir="$1"
 shift
 set -eu
 
-if [ -z "$version_tag" ] || [ -z "$project_name" ]; then
-  echo "Syntax: save_patches.sh <version_tag> <project_name>"
+if [ -z "$version_tag" ] || [ -z "$project_name" ] || [ -z "$patch_subdir" ]; then
+  echo "Syntax: save_patches.sh <version_tag> <project_name> <patch_subdir>"
   exit 1
 fi
 
 # Directories.
-patch_dir="$root_dir/patches/$version_tag/$project_name"
+patch_dir="$root_dir/patches/$patch_subdir/$project_name"
 mkdir -p $patch_dir
 source_dir="$root_dir/sources/$project_name"
 if ! [ -d "$source_dir" ]; then
