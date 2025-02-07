f3819aec711056422406dd136d7a6334e1f15f64
Stella Laurenzo <stellaraccident@gmail.com>
2025-02-06 18:33:24 -0800

    Tweak so test runs on non-GPU machines

--- examples/cpp-sdk-user/hip-host-test.cpp
+++ examples/cpp-sdk-user/hip-host-test.cpp
@@ -4,16 +4,18 @@
 
 int main(int argc, char **argv) {
   int runtime_version;
-  auto err = hipInit(0);
-  if (err != hipSuccess) {
-    std::cerr << "Error initializing HIP: " << err << "\n";
-    return 1;
-  }
-  err = hipRuntimeGetVersion(&runtime_version);
+  auto err = hipRuntimeGetVersion(&runtime_version);
   if (err != hipSuccess) {
     std::cerr << "Error getting runtime version: " << err << "\n";
     return 2;
   }
   std::cout << "HIP runtime version: " << runtime_version << "\n";
+  err = hipInit(0);
+  if (err != hipSuccess) {
+    // Since this is primarily run on CI to verify that linkage works, we
+    // just ignore any error (this lets us run the test on machines that
+    // lack a GPU).
+    std::cerr << "Error initializing HIP: " << err << " (ignored)\n";
+  }
   return 0;
 }
