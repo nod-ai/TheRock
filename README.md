a0edfd0c46d16b416dfd12d23da6a2c2390cbc71
Stella Laurenzo <stellaraccident@gmail.com>
2025-02-10 20:25:28 -0800

    Fully parameterize the build.
    * Options are defined for every library.
    * Dependencies are specified so that it is safe to enable/disable any subset and have the build do the right thing.
    * Updates tests to adapt to feature availability.
    * Updates documentation, removing some obsolete docs and defining the flags.

--- README.md
+++ README.md
@@ -79,45 +79,50 @@ cmake -B build -GNinja . -DTHEROCK_AMDGPU_FAMILIES=gfx110X-dgpu
 # Or if iterating and wishing to cache:
 #   cmake -Bbuild -GNinja -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache .
 cmake --build build
-
-# Install per component.
-cmake --install build --component amdgpu-runtime
-cmake --install build --component amdgpu-runtime-dev
-cmake --install build --component amdgpu-compiler
-
-# Create archives.
-(cd build && cpack -G TBZ2)
 ```
+
+## Feature Flags
+
+By default, the project builds everything available. The following group flags
+allow enable/disable of selected subsets:
+
+- `-DTHEROCK_ENABLE_ALL=OFF`: Disables all optional components.
+- `-DTHEROCK_ENABLE_CORE=OFF`: Disables all core components.
+- `-DTHEROCK_ENABLE_COMM_LIBS=OFF`: Disables all communication libraries.
+- `-DTHEROCK_ENABLE_MATH_LIBS=OFF`: Disables all math libraries.
+- `-DTHEROCK_ENABLE_ML_LIBS=OFF`: Disables all ML libraries.
+
+Individual features can be controlled separately (typically in combination with
+`-DTHEROCK_ENABLE_ALL=OFF` or `-DTHEROCK_FORCE_DISABLE_ALL=ON` to force a
+minimal build):
+
+- `-DTHEROCK_ENABLE_COMPILER=ON`: Enables the GPU+host compiler toolchain.
+- `-DTHEROCK_ENABLE_HIPIFY=ON`: Enables the hipify tool.
+- `-DTHEROCK_ENABLE_CORE_RUNTIME=ON`: Enables the core runtime components and tools.
+- `-DTHEROCK_ENABLE_HIP_RUNTIME=ON`: Enables the HIP runtime components.
+- `-DTHEROCK_ENABLE_RCCL=ON`: Enables RCCL.
+- `-DTHEROCK_ENABLE_PRIM=ON`: Enables the PRIM library.
+- `-DTHEROCK_ENABLE_BLAS=ON`: Enables the BLAS libraries.
+- `-DTHEROCK_ENABLE_RAND=ON`: Enables the RAND libraries.
+- `-DTHEROCK_ENABLE_SOLVER=ON`: Enables the SOLVER libraries.
+- `-DTHEROCK_ENABLE_SPARSE=ON`: Enables the SPARSE libraries.
+- `-DTHEROCK_ENABLE_MIOPEN=ON`: Enables MIOpen.
+
+Enabling any features will implicitly enable its *minimum* dependencies. Some
+libraries (like MIOpen) have a number of *optional* dependencies, which must
+be enabled manually if enabling/disabling individual features.
+
+A report of enabled/disabled features and flags will be printed on every
+CMake configure.
 
 ## Sanity Checks
 
-The following does not replace a robust test suite. However, it will tell you
-whether the toolchain you have just crafted is viable at all (as in can load
-and enumerate devices).
+Tests of the integrity of the build are enabled by default and can be run
+with ctest:
 
 ```
-./build/dlopen-hip install/lib/libamdhip64.so
+ctest --test-dir build
 ```
 
-HIP enabled IREE can also be used to enumerate:
-
-```
-LD_LIBRARY_PATH=install/lib iree-run-module --dump_devices=hip
-```
-
-# Development Notes
-
-## Building in a manylinux container
-
-Our CI builds run in such a container, and it can be useful to run locally.
-
-```
-docker run --rm -it -v $PWD:$PWD --entrypoint /bin/bash ghcr.io/nod-ai/manylinux_ghr_x86_64:main
-```
-
-Packages needed:
-
-```
-yum install -y numactl-devel elfutils-libelf-devel vim-common git-lfs
-pip install CppHeaderParser
-```
+Testing functionality on an actual GPU is in progress and will be documented
+separately.
---
+++ cmake/therock_features.cmake
@@ -0,0 +1,92 @@
+# therock_features.cmake
+# Registry of features that can be enabled/disabled when building TheRock.
+# In this context, "feature" can refer to either a top-level component/project
+# or some optional configuration of it (typically as "project_feature").
+# Features can have dependencies, which are a superset of the project
+# dependencies and must be kept rationalized by hand.
+#
+# Each feature results in the following changes to the caller scope:
+#   THEROCK_ENABLE_${feature_name}: boolean of whether enabled/disabled
+#   THEROCK_REQUIRES_${feature_name}: List of features that are required to
+#     be enabled for this feature
+#   THEROCK_ALL_FEATURES: Added to this global list of features.
+#
+# A feature is enabled by default unless if the GROUP keyword is specified.
+# In this case, it's default state is dependent on THEROCK_ENABLE_${group}.
+
+function(therock_add_feature feature_name)
+  cmake_parse_arguments(PARSE_ARGV 1 ARG
+    ""
+    "GROUP;DESCRIPTION"
+    "REQUIRES"
+  )
+
+  set(_default_enabled ON)
+  if(ARG_GROUP)
+    if(NOT "${THEROCK_ENABLE_${ARG_GROUP}}")
+      set(_default_enabled OFF)
+    endif()
+  endif()
+  if(THEROCK_FORCE_DISABLE_ALL)
+    set(_default_enabled OFF)
+    set(_force "FORCE")
+  endif()
+
+  # Validate.
+  if("${feature_name}" IN_LIST THEROCK_ALL_FEATURES)
+    message(FATAL_ERROR "CMake feature already defined: ${feature_name}")
+  endif()
+  foreach(require ${ARG_REQUIRES})
+    if(NOT DEFINED THEROCK_ENABLE_${require})
+      message(FATAL_ERROR "CMake feature order error: ${feature_name} requires ${require} which was not defined first")
+    endif()
+  endforeach()
+
+  # Set up the cache option and inject the effective value into the parent
+  # scope.
+  set(THEROCK_ENABLE_${feature_name} ${_default_enabled} CACHE BOOL "${ARG_DESCRIPTION}" ${_force})
+  set(_actual $CACHE{THEROCK_ENABLE_${feature_name}})
+  set(THEROCK_ENABLE_${feature_name} "${_actual}" PARENT_SCOPE)
+  set(THEROCK_REQUIRES_${feature_name} ${ARG_REQUIRES} PARENT_SCOPE)
+  set(_all_features ${THEROCK_ALL_FEATURES})
+  list(APPEND _all_features "${feature_name}")
+  set(THEROCK_ALL_FEATURES ${_all_features} PARENT_SCOPE)
+endfunction()
+
+function(therock_finalize_features)
+  # Force enable any features required of an enabled feature.
+  # These are processed in reverse declaration order, which ensures a DAG.
+  set(all_features_reversed ${THEROCK_ALL_FEATURES})
+  list(REVERSE all_features_reversed)
+  foreach(feature_name ${THEROCK_ALL_FEATURES})
+    if(THEROCK_ENABLE_${feature_name})
+      foreach(require ${THEROCK_REQUIRES_${feature_name}})
+        set(THEROCK_ENABLE_${require} ON)
+        set(THEROCK_ENABLE_${require} ON PARENT_SCOPE)
+      endforeach()
+    endif()
+  endforeach()
+
+  # And report.
+  message(STATUS "Enabled features:")
+  set(_available_list)
+  foreach(feature_name ${THEROCK_ALL_FEATURES})
+    if(THEROCK_ENABLE_${feature_name})
+      message(STATUS "  * ${feature_name} (-DTHEROCK_ENABLE_${feature_name}=ON)")
+    else()
+      list(APPEND _available_list "${feature_name}")
+    endif()
+  endforeach()
+  if(_available_list)
+    message(STATUS "Disabled features:")
+    foreach(feature_name ${_available_list})
+      message(STATUS "  * ${feature_name} (-DTHEROCK_ENABLE_${feature_name}=OFF)")
+    endforeach()
+  endif()
+
+  # Reset the force disable flag.
+  if(THEROCK_FORCE_DISABLE_ALL)
+    set(THEROCK_FORCE_DISABLE_ALL OFF CACHE BOOL "" FORCE)
+    set(THEROCK_FORCE_DISABLE_ALL OFF PARENT_SCOPE)
+  endif()
+endfunction()
