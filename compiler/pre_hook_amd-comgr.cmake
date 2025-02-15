31a9888cb973705803fe2b0a0fdb832eaeb11cf3
Scott Todd <scott.todd0@gmail.com>
2025-02-14 16:43:27 -0800

    Enable compiler subprojects on Windows. (#95)
    Progress on https://github.com/nod-ai/TheRock/issues/36.
    
    We'll want to switch to self-hosted runners and tune the cache to make
    the build faster soon. This is still under 20 minutes on standard
    runners though:
    https://github.com/nod-ai/TheRock/actions/runs/13339298280/job/37260996314.

---
+++ compiler/pre_hook_amd-comgr.cmake
@@ -0,0 +1,9 @@
+if(WIN32)
+  set(LLVM_LINK_LLVM_DYLIB OFF)
+else()
+  set(LLVM_LINK_LLVM_DYLIB ON)
+endif()
+
+# The comgr tests have a circular dependency on the HIP runtime.
+# https://github.com/nod-ai/TheRock/issues/67
+set(BUILD_TESTING OFF CACHE BOOL "DISABLE BUILDING TESTS IN SUBPROJECTS" FORCE)
