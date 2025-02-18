716a9e53bd8fbb863130b1ef0d93438e4f5930af
Stella Laurenzo <slaurenz@amd.com>
2025-02-17 17:54:26 -0800

    Consolidate BLAS libraries and enable solver in hipBLAS. (#100)
    * Eliminates separate artifacts for solver/sparse.
    * Wires up optionality of SOLVER for hipBLAS.
    * Moves all BLAS libraries to one sub-directory for easier management.
    * A future refactoring will create one CMake project to include them
    all, optimizing the build graph of these critical path components.

---
+++ math-libs/BLAS/README.md
@@ -0,0 +1,16 @@
+# ROCM BLAS Libraries
+
+This directory contains the suite of projects which collectively implement
+ROCM BLAS APIs on AMD GPUs.
+
+Some libraries are low-level, implementation libraries. The following are
+user-level API libraries:
+
+* hipBLASLt
+* hipBLAS
+* hipSOLVER
+* hipSPARSE
+
+Currently, all component libraries are built individually, but a future rework
+will nest them all under the same CMake project for better optimization of the
+build graph.
--- ml-libs/pre_hook_MIOpen.cmake
+++ ml-libs/pre_hook_MIOpen.cmake
@@ -7,10 +7,10 @@ list(APPEND CMAKE_SYSTEM_INCLUDE_PATH "${THEROCK_BINARY_DIR}/base/half/stage/inc
 # in an isolated directory via find_package (this has probably been masked when
 # in a directory with everything else). So we just include it here.
 # Fix upstream and remove this.
-include_directories("${THEROCK_BINARY_DIR}/math-libs/hipBLAS/stage/include")
+include_directories("${THEROCK_BINARY_DIR}/math-libs/BLAS/hipBLAS/stage/include")
 
 # TODO: Ditto for hipBLAS-common
-include_directories("${THEROCK_BINARY_DIR}/math-libs/hipBLAS-common/stage/include")
+include_directories("${THEROCK_BINARY_DIR}/math-libs/BLAS/hipBLAS-common/stage/include")
 
 # TODO: Ditto for rocrand
 include_directories("${THEROCK_BINARY_DIR}/math-libs/rocRAND/stage/include")
