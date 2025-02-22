6b52c37f8a3be7a3835df490f8bf61dfdb18132e
Stella Laurenzo <stellaraccident@gmail.com>
2025-02-22 14:26:28 -0800

    Add more libraries

--- third-party/sysdeps/linux/numactl/patch_install.sh
+++ third-party/sysdeps/linux/numactl/patch_install.sh
@@ -8,18 +8,14 @@ Python3_EXECUTABLE="${Python3_EXECUTABLE:?Python3_EXECUTABLE not defined}"
 
 "$Python3_EXECUTABLE" "$THEROCK_SOURCE_DIR/build_tools/patch_linux_so.py" \
   --patchelf "${PATCHELF}" --add-prefix rocm_sysdeps_ \
-    $PREFIX/lib/libelf.so \
-    $PREFIX/lib/libdw.so \
-    $PREFIX/lib/libasm.so
+  $PREFIX/lib/libnuma.so
 
 # pc files are not output with a relative prefix. Sed it to relative.
 sed -i -E 's|^prefix=.+|prefix=${pcfiledir}/../..|' $PREFIX/lib/pkgconfig/*.pc
+sed -i -E 's|^exec_prefix=.+|exec_prefix=${pcfiledir}/../..|' $PREFIX/lib/pkgconfig/*.pc
+sed -i -E 's|^libdir=.+|libdir=${prefix}/lib|' $PREFIX/lib/pkgconfig/*.pc
+sed -i -E 's|^includedir=.+|includedir=${prefix}/include|' $PREFIX/lib/pkgconfig/*.pc
 
-# We don't need old v0 compat libs.
-rm -f $PREFIX/lib/*-0.so
-
-# We don't want the static libs.
-rm $PREFIX/lib/*.a
-
-# We don't need the programs.
+# We don't want library descriptors or binaries.
+rm $PREFIX/lib/*.la
 rm -Rf $PREFIX/bin
