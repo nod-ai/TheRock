839df9734120ce6664aa1aa5a221bf3693c9775f
geomin12 <geomin12@amd.com>
2025-03-05 15:13:46 -0800

    Linting

--- tests/hip_printf.cpp
+++ tests/hip_printf.cpp
@@ -1,15 +1,15 @@
 #include <cstdio>
 #include <hip/hip_runtime.h>
-__global__ void squares(int* buf){
+__global__ void squares(int *buf) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
-  printf("Thread %#04x is writing %4d\n", i, i*i);
+  printf("Thread %#04x is writing %4d\n", i, i * i);
   buf[i] = i * i;
 }
-int main(){
+int main() {
   constexpr int gridsize = 1;
   constexpr int blocksize = 64;
   constexpr int size = gridsize * blocksize;
-  int* d_buf;
+  int *d_buf;
   hipMalloc(&d_buf, size * sizeof(int));
   hipLaunchKernelGGL(squares, gridsize, blocksize, 0, 0, d_buf);
   hipDeviceSynchronize();
--- tests/test_rocm_sanity.py
+++ tests/test_rocm_sanity.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 THIS_DIR = Path(__file__).parent
 
+
 def run_command_with_search(command, to_search):
     process = subprocess.run(command, capture_output=True, check=False)
     if process.returncode != 0:
@@ -15,22 +16,35 @@ def run_command_with_search(command, to_search):
 class TestROCmSanity:
     def test_rocminfo(self):
         assert run_command_with_search(["rocminfo"], r"Device\s*Type:\s*GPU")
-        
+
         assert run_command_with_search(["rocminfo"], r"Name:\s*gfx")
-        
+
         assert run_command_with_search(["rocminfo"], r"Vendor\s*Name:\s*AMD")
 
     def test_clinfo(self):
-        assert run_command_with_search(["clinfo"], r"Device(\s|\\t)*Type:(\s|\\t)*CL_DEVICE_TYPE_GPU")
-        
+        assert run_command_with_search(
+            ["clinfo"], r"Device(\s|\\t)*Type:(\s|\\t)*CL_DEVICE_TYPE_GPU"
+        )
+
         assert run_command_with_search(["clinfo"], r"Name:(\s|\\t)*gfx")
-        
-        assert run_command_with_search(["clinfo"], r"Vendor:(\s|\\t)*Advanced Micro Devices, Inc.")
-        
+
+        assert run_command_with_search(
+            ["clinfo"], r"Vendor:(\s|\\t)*Advanced Micro Devices, Inc."
+        )
+
     def test_hip_printf(self):
         # Compiling .cpp file using hipcc
-        run_command_with_search(["hipcc", f"{str(THIS_DIR)}/hip_printf.cpp", "-o",  f"{str(THIS_DIR)}/hip_printf"], "")
-        
+        run_command_with_search(
+            [
+                "hipcc",
+                f"{str(THIS_DIR)}/hip_printf.cpp",
+                "-o",
+                f"{str(THIS_DIR)}/hip_printf",
+            ],
+            "",
+        )
+
         # Running the executable
-        assert run_command_with_search([f"{str(THIS_DIR)}/hip_printf"], r"Thread......is\swriting")
-        
+        assert run_command_with_search(
+            [f"{str(THIS_DIR)}/hip_printf"], r"Thread......is\swriting"
+        )
