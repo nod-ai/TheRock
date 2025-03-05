05d8679f0dde23acb68a9257f48448e8fe9f05e3
Marius Brehler <marius.brehler@amd.com>
2025-03-05 22:25:59 +0100

    Update URLs following ROCm move (#152)
    The repository was moved from [`nod-ai`](https://github.com/nod-ai) to
    the [`ROCm`](https://github.com/ROCm) organization.

--- core/post_hook_hip-clr.cmake
+++ core/post_hook_hip-clr.cmake
@@ -1,4 +1,4 @@
-# See: https://github.com/nod-ai/TheRock/issues/21
+# See: https://github.com/ROCm/TheRock/issues/21
 if(HIPCC_BIN_DIR)
   message(FATAL_ERROR "The legacy HIPCC_BIN_DIR was somehow set, indicating a bug in the clr CMake files: ${HIPCC_BIN_DIR}")
 endif()
