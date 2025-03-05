ad584175d459911529eed16739d1cb7f0ea25d49
geomin12 <geomin12@amd.com>
2025-03-05 10:21:44 -0800

    Initial rocm tests here - to test gh action

---
+++ tests/test_rocm_sanity.py
@@ -0,0 +1,28 @@
+import pytest
+import subprocess
+import re
+
+def run_command_with_search(command, to_search):
+    process = subprocess.run(command, capture_output=True, check=False)
+    if process.returncode != 0:
+        pytest.fail(f"The command {command} failed. Failing this test...")
+    return re.search(to_search, str(process.stdout))
+
+
+class TestROCmSanity:
+    def test_rocminfo(self):
+        assert run_command_with_search(["rocminfo"], r"Device\s*Type:\s*GPU")
+        
+        assert run_command_with_search(["rocminfo"], r"Name:\s*gfx")
+        
+        assert run_command_with_search(["rocminfo"], r"Vendor\s*Name:\s*AMD")
+        
+        # L2 Cache size >= GPU marketing name
+
+    def test_clinfo(self):
+        assert run_command_with_search(["clinfo"], r"Device(\s|\\t)*Type:(\s|\\t)*CL_DEVICE_TYPE_GPU")
+        
+        assert run_command_with_search(["clinfo"], r"Name:(\s|\\t)*gfx")
+        
+        assert run_command_with_search(["clinfo"], r"Vendor:(\s|\\t)*Advanced Micro Devices, Inc.")
+        
