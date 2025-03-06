a96dba5c809107ed8123a9e5913fb4881762b41e
geomin12 <geomin12@amd.com>
2025-03-06 11:23:57 -0800

    Adding more description to tests and cleanup

--- tests/test_rocm_sanity.py
+++ tests/test_rocm_sanity.py
@@ -5,44 +5,47 @@ from pathlib import Path
 
 THIS_DIR = Path(__file__).resolve().parent
 
-
 def run_command(command):
     process = subprocess.run(command, capture_output=True, check=False)
     if process.returncode != 0:
         pytest.fail(f"The command {command} failed. Failing this test...")
     return str(process.stdout)
 
+rocm_info_output = run_command(["rocminfo"])
+cli_info_output = run_command(["clinfo"])
 
 class TestROCmSanity:
-    def test_rocminfo(self):
-        output = run_command(["rocminfo"])
+    
+    @pytest.mark.parametrize("to_search, output", [
+        (r"Device\s*Type:\s*GPU", rocm_info_output),
+        (r"Name:\s*gfx", rocm_info_output),
+        (r"Vendor\s*Name:\s*AMD", rocm_info_output),
+        (r"Device(\s|\\t)*Type:(\s|\\t)*CL_DEVICE_TYPE_GPU", cli_info_output),
+        (r"Name:(\s|\\t)*gfx", cli_info_output),
+        (r"Vendor:(\s|\\t)*Advanced Micro Devices, Inc.", cli_info_output)
+    ], ids = [
+        "rocminfo - GPU Device Type Search",
+        "rocminfo - GFX Name Search",
+        "rocminfo - AMD Vendor Name Search",
+        "clinfo - GPU Device Type Search",
+        "clinfo - GFX Name Search",
+        "clinfo - AMD Vendor Name Search",
+    ])
+    def test_info_output(self, to_search, output):
+        pytest.assume(re.search(to_search, output))
 
-        pytest.assume(re.search(r"Device\s*Type:\s*GPU", output))
-        pytest.assume(re.search(r"Name:\s*gfx", output))
-        pytest.assume(re.search(r"Vendor\s*Name:\s*AMD", output))
-
-    def test_clinfo(self):
-        output = run_command(["clinfo"])
-
-        pytest.assume(
-            re.search(r"Device(\s|\\t)*Type:(\s|\\t)*CL_DEVICE_TYPE_GPU", output)
-        )
-        pytest.assume(re.search(r"Name:(\s|\\t)*gfx", output))
-        pytest.assume(
-            re.search(r"Vendor:(\s|\\t)*Advanced Micro Devices, Inc.", output)
-        )
 
     def test_hip_printf(self):
         # Compiling .cpp file using hipcc
         run_command(
             [
                 "hipcc",
-                f"{str(THIS_DIR)}/hip_printf.cpp",
+                str(THIS_DIR / "hip_printf.cpp"),
                 "-o",
-                f"{str(THIS_DIR)}/hip_printf",
+                str(THIS_DIR / "hip_printf"),
             ]
         )
 
         # Running the executable
-        output = run_command([f"{str(THIS_DIR)}/hip_printf"])
+        output = run_command([str(THIS_DIR / "hip_printf")])
         pytest.assume(re.search(r"Thread.*is\swriting", output))
